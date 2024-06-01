33604496ccb4ac284919217bcfac65cc41d6ab8a
Adrian Chege <achege116@gmail.com>
2024-06-02 02:38:08 +0300

    Made the navbar responsive for smaller screens and added the hamburger icon to toggle the navbar on smaller screeens

--- React-Portfolio-App/src/components/Navbar/Navbar.jsx
+++ React-Portfolio-App/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,28 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.webp'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
-
+import menu from '../../assets/menu.svg'
+import close_icon from '../../assets/close_icon.svg'
 
 
 const Navbar = () => {
 
-  const [menu, setMenu] = useState("home")
+  const menuRef = useRef();
+
+  const openMenu = ()=>{
+    menuRef.current.style.right="0"
+  }
+  const closeMenu = ()=>{
+    menuRef.current.style.right="-350px"
+  }
 
   return (
     <div className='navbar'>
         <img src={logo} alt="" />
-        <ul className="nav-menu">
+        <img src={menu} alt='' className='nav-mobile-open' onClick={openMenu}/>
+        <ul ref={menuRef} className="nav-menu">
+          <img src={close_icon} alt="" className="nav-mobile-close" onClick={closeMenu} />
             <li className="anchor" ><AnchorLink className='anchor-link' offset={50} href='#hero'>Home</AnchorLink> </li>
             <li className="anchor"><AnchorLink className='anchor-link' offset={50} href='#about'>About Me</AnchorLink></li>
             <li className="anchor"><AnchorLink className='anchor-link' offset={50} href='#services'>Services</AnchorLink></li>
