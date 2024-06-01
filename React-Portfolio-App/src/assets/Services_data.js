7176fe88edfb5f1244630df5c280de4b5e4e38c7
Adrian Chege <achege116@gmail.com>
2024-06-01 05:12:23 +0300

    This are images tha have been used in the application

---
+++ React-Portfolio-App/src/assets/Services_data.js
@@ -0,0 +1,14 @@
+const data = [
+    {
+        s_no: "01",
+        s_name: "Front-end Development",
+        s_desc: "I am well capable of creating and designing user interfaces for web applications. With the use of HTML/CSS and frameworks and librarires such as React JS and bootstrap, I create dynamic and user friendly websites enjoyed by all"
+    },
+    {
+        s_no: "02",
+        s_name: "Back-end Development",
+        s_desc: "I am able to manipualte backend technologies such as Python and JavaScript to manage user data. I am also equipped with knowledge in MySQL and backend frameworks like Flask. I am able to collect and manage user data."
+    }
+]
+
+export default data
